68c3c7c0ddd88ad2a02c1b32faa3ed15993dd103
malashenok <a.malashenok@inbox.ru>
2021-04-18 14:31:24 +0300

    init

---
+++ index.js
@@ -0,0 +1,8 @@
+//import 'whatwg-fetch'
+//import 'promise-polyfill/src/polyfill'
+import mainObj from 'js/main'
+import 'style/style.css'
+import 'style/normalize.css'
+
+
+let app = new Vue(mainObj)
